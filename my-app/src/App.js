31798a5388f67b0c8fbeb7877545269f25c34dbc
caesiumy <mn0658@naver.com>
2019-11-15 23:20:36 +0900

    feat: Add 3rd week 1. props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+const WorldTime = props => {
+  return (
+    <div className="worldTime">
+      <h2 className="world">
+        <span role="img" aria-label="world" className="worldImg">
+          🌍
+        </span>{" "}
+        {props.city}
+      </h2>
+      <h2 className="time">
+        <span role="img" aria-label="time" className="timeImg">
+          ⏰
+        </span>{" "}
+        시간: {props.time} 시
+      </h2>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Hello world!</h1>
+      <WorldTime city={"서울"} time={11} />
+      <WorldTime city={"베이징"} time={10} />
+      <WorldTime city={"시드니"} time={13} />
+      <WorldTime city={"LA"} time={18} />
     </div>
   );
 }
