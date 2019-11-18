7cd7d6f2239dddb4ca84d09d0ca3cb3104d8b619
caesiumy <mn0658@naver.com>
2019-11-18 23:37:20 +0900

    feat: Add axios & api.js

---
+++ rest-front/src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+
+export default {
+  getAllPosts() {
+    return axios.get("/posts/");
+  },
+
+  createPost(payload) {
+    return axios.post("/posts/", payload);
+  }
+};
