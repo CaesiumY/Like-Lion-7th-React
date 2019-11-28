698f7995d387d26599fe751b34279768c1ebc76d
caesiumy <mn0658@naver.com>
2019-11-29 00:32:21 +0900

    style: Add cards

--- rest-front/src/components/PostView.js
+++ rest-front/src/components/PostView.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import api from "../api";
 
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
 export default class postView extends Component {
-  handleDelete = async e => {
-    await api.deletePost(e.target.value);
+  handleDelete = async (payload, e) => {
+    console.log(payload, e.target);
+    await api.deletePost(payload.id);
     this.props.getPosts();
   };
 
@@ -11,15 +18,26 @@ export default class postView extends Component {
     const { title, body, id } = this.props;
 
     return (
-      <div>
-        <p>{id}</p>
-        <h3>{title}</h3>
-        <p>{body}</p>
-        <button value={id} onClick={this.handleDelete}>
-          삭제하기
-        </button>
-        <hr />
-      </div>
+      <Card className={"card"}>
+        <CardContent>
+          <Typography color="textSecondary">{id}</Typography>
+          <Typography variant="h5">{title}</Typography>
+          <Typography variant="body2" component="p">
+            {body}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <hr />
+          <Button
+            color="secondary"
+            variant="outlined"
+            size="small"
+            onClick={this.handleDelete.bind(this, { id })}
+          >
+            삭제하기
+          </Button>
+        </CardActions>
+      </Card>
     );
   }
 }
